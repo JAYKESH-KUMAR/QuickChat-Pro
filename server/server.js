8e2267f4a8e6856e8a027180e635c3691de2a7ad
Jaykesh Kumar <jaykeshkumar549@gmail.com>
2026-06-22 13:26:14 +0530

    Final CORS fix

--- server/server.js
+++ server/server.js
@@ -46,7 +46,10 @@ io.on("connection", (socket) => {
 // Middleware setup
 app.use(express.json({ limit: "4mb" }));
 
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 
 app.use("/api/status", (req, res) => res.send("Server is live "));
 app.use("/api/auth", userRouter);
